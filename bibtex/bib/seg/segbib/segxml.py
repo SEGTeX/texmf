f36af6fcea4de64535e0ee8aba73d7d8e4ba906c
Sergey Fomel <sergey.fomel@gmail.com>
2008-06-26 19:32:27 +0000

    update

--- bibtex/bib/seg/segbib/segxml.py
+++ bibtex/bib/seg/segbib/segxml.py
@@ -3,5 +3,17 @@
 from elementtree.ElementTree import ElementTree
 import glob
 
-for xml in glob.glob('DCI_Archive/*.xml'):
-    doc = ElementTree(file=xml)
+#for xml in glob.glob('DCI_Archive/*.xml'):
+for xml in ('DCI_Archive/EAE_2006_F002.xml',):
+    tree = ElementTree(file=xml)
+    for elem in tree.getroot():
+        print elem.tag, ' => ', elem.text
+        for subelem in elem:
+            print ': ', subelem.tag, '=> ', subelem.text
+            for subsubelem in subelem:
+                print ':: ', subsubelem.tag, '=> ', subsubelem.text
+                for subsubsubelem in subsubelem:
+                    print '::: ', subsubsubelem.tag, '=> ', subsubsubelem.text
+
+
+    
